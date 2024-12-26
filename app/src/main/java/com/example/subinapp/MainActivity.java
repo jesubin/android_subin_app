66509b3400611036838442b8c212093d8c29f149
myname <dokdokorea01@naver.com>
2024-12-26 16:40:10 +0900

    getAllUser 메서드 추가

--- app/src/main/java/com/example/subinapp/MainActivity.java
+++ app/src/main/java/com/example/subinapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.subinapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -53,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
                 }
 
 
+        );
+
+        ButtonUserlist.setOnClickListener(
+             new View.OnClickListener(){
+                 @Override
+                 public void onClick(View v){
+                     Intent intent = new Intent(MainActivity.this, UserlistActivity.class);
+                     startActivity(intent);
+                 }
+             }
         );
     }
 
