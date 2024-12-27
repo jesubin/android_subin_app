2a6c1dea00022cddfbe6ebfccf03420172118a11
myname <dokdokorea01@naver.com>
2024-12-27 12:04:52 +0900

    로그인 성공여부에 따른 처리 구현

---
+++ app/src/main/java/com/example/subinapp/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.subinapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.subinapp.helper.DatabaseHelper;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // 변수 선언
+    private EditText editTextUserid, editTextPasswd;
+    private Button buttonLogin;
+    private DatabaseHelper databaseHelper;
+    private SharedPreferences sharedPreferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // 변수 초기화
+        editTextUserid = findViewById(R.id.editTextUserid);
+        editTextPasswd = findViewById(R.id.editTextPasswd);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        databaseHelper = new DatabaseHelper(this);
+        sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
+
+        // 로그인 이벤트 처리
+        buttonLogin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                      String userid = editTextUserid.getText().toString();
+                      String passwd = editTextPasswd.getText().toString();
+
+                      if(databaseHelper.loginUser(userid, passwd)) {
+                          //로그인 성공시 SharedPreferences 세션 데이터 저장
+                          SharedPreferences.Editor editor = sharedPreferences.edit();
+                          editor.putBoolean("isLoggedIn", true);
+                          editor.apply();
+
+                          Intent intent = new Intent(LoginActivity.this, UserlistActivity.class);
+                          startActivity(intent);
+                      }else{
+                          Toast.makeText(LoginActivity.this,"로그인 실패 아이디/비밀번호를 확인하세요", Toast.LENGTH_SHORT).show();
+                      }
+                    }
+                }
+        );
+    }
+}
